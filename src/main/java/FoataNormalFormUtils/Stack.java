75419a6ba79a55bd24a2b5c9e9fa7acf80a8bee3
Anna Marciniec <annamarciniec24601@gmail.com>
2017-11-30 21:47:57 +0100

    updated gitignore and stack

--- src/main/java/FoataNormalFormUtils/Stack.java
+++ src/main/java/FoataNormalFormUtils/Stack.java
@@ -1,4 +1,14 @@
 package FoataNormalFormUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    private List<String> letterStack;
+    Stack(){
+        letterStack = new ArrayList<String>();
+    }
+    public void push(String element){
+
+    }
 }
